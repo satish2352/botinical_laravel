63215d276036afb9dc848ec4a9dd7a537633bb16
Savita444 <savita.hajare@sumagoinfotech.com>
2024-05-02 17:55:44 +0530

    change in login api

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -37,16 +37,34 @@ class AuthController extends Controller
 
     //     return $this->respondWithToken($token);
     // }
-    public function login(){
+    public function login(Request $request){
         try {
-            $credentials = request()->validate([
-                'mobile_number' => 'required|string'
-            ]);
-            
-            // Find or create the user
-            // $user = User::firstOrNew(['mobile_number' => $credentials['mobile_number']]);
-            
-             // Check if user exists with the provided mobile number
+            // $credentials = request()->validate([
+            //     'mobile_number' => ['required', 'string', 'digits:10', 'regex:/^[6-9]\d{9}$/']
+            // ]);
+                  
+            $all_data_validation = [
+                'mobile_number' => ['required', 'digits:10', 'regex:/^[6789]\d{9}$/'],
+            ];
+
+            $customMessages = [
+                'mobile_number.required' => 'Mobile number is required.',
+                'mobile_number.digits' => 'Mobile number must be 10 digits.',
+                'mobile_number.regex' => 'Mobile number must start with 9, 8, 7 or 6.',
+            ];
+    
+            $validator = Validator::make($request->all(), $all_data_validation, $customMessages);
+          
+           if ($validator->fails()) {
+                $errors = $validator->errors()->all();
+                $errorMessage = implode(" \n ", $errors);
+                return response()->json([
+                    'status' => 'false',
+                    'message' => 'Validation Fail: ' . $errorMessage,
+                ], 200);
+            }
+    
+            // Check if user exists with the provided mobile number
             $user = User::where('mobile_number', $credentials['mobile_number'])->first();
             
             if(!$user) {
@@ -54,19 +72,16 @@ class AuthController extends Controller
                 $user = new User();
                 $user->mobile_number = $credentials['mobile_number'];
             }
-
+    
             // $otp = mt_rand(1000, 9999); 
             $otp = "1234";
             $user->user_otp = $otp;
             $user->save();
             
-            // Generate JWT token
             $token = auth()->login($user);
     
-            // Update remember_token
             $user->update(['remember_token' => $token]);
             
-            // Return response with token
             return response()->json([
                 'status' => 'true',
                 'message' => 'Login successful',
@@ -79,6 +94,7 @@ class AuthController extends Controller
             return response()->json(['status' => 'false','message' => 'Login fail', 'error' => $e->getMessage()], 500);
         }
     }
+    
     
     public function verifyOTP(Request $request){
         try {
