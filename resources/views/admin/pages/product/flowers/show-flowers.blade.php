3188196492d57c2e332ed4c591127be0419f2ca3
Savita444 <savita.hajare@sumagoinfotech.com>
2024-05-13 11:00:31 +0530

    change in admin

--- resources/views/admin/pages/product/flowers/show-flowers.blade.php
+++ resources/views/admin/pages/product/flowers/show-flowers.blade.php
@@ -100,7 +100,7 @@
                                             <label> वीडियो :</label>
                                         </div>
                                         <div class="col-lg-8 col-md-8 col-sm-8">
-                                            <video width="300" height="150" controls>
+                                            <video id="hindi_videoPreview" width="300" height="150" controls>
                                                 <source src="{{ Config::get('DocumentConstant.FLOWERS_VIEW') }}{{ $flowers->hindi_video_upload }}" type="video/mp4">
                                             </video>
                                         </div>
