40534c456679fa5f4fa9d72e4f136b8bb86423cf
Savita444 <savita.hajare@sumagoinfotech.com>
2024-05-21 11:01:12 +0530

    changes

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -109,7 +109,7 @@
         </div>
         <div class="row">
             <div class="col-md-12 col-md-12 col-sm-12 col-xs-12 text-center">
-                <p>Copyright &copy; {{date('Y')}} <a href="#">botinical</a> All rights reserved.</p>
+                <p>Copyright &copy; {{date('Y')}} <a href="#">botanical</a> All rights reserved.</p>
             </div>
         </div>
     </div>
