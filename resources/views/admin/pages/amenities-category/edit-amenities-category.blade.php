bb97c471fa504aff3761b74e7b32b658a67386b2
Savita444 <savita.hajare@sumagoinfotech.com>
2024-05-22 09:53:28 +0530

    changes

--- resources/views/admin/pages/amenities-category/edit-amenities-category.blade.php
+++ resources/views/admin/pages/amenities-category/edit-amenities-category.blade.php
@@ -12,7 +12,7 @@
                 <div class="sparkline12-hd">
                     <div class="main-sparkline12-hd">
                         <center>
-                            <h1>Add Amenities Category Data</h1>
+                            <h1>Edit Amenities Category Data</h1>
                         </center>
                     </div>
                 </div>
