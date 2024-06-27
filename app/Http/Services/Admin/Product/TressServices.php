7bedee7482702fb67eb6d8d65ba8641766c0b00d
Savita444 <savita.hajare@sumagoinfotech.com>
2024-06-27 19:15:10 +0530

    changes in tress

--- app/Http/Services/Admin/Product/TressServices.php
+++ app/Http/Services/Admin/Product/TressServices.php
@@ -32,10 +32,7 @@ class TressServices
    
     public function addAll($request){
         try {
-            $last_id = $this->repo->addAll($request); 
-            
-            dd($last_id);
-            die();
+            $last_id = $this->repo->addAll($request);        
             if(isset($last_id['ImageName'])){
                 $path = Config::get('DocumentConstant.TRESS_ADD');
                 $ImageName = $last_id['ImageName'];
