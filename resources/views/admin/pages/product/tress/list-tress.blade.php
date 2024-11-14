511356437678b704d0d7dd17435e531ecda0fb20
Savita444 <savita.hajare@sumagoinfotech.com>
2024-11-14 12:49:59 +0530

    changes

--- resources/views/admin/pages/product/tress/list-tress.blade.php
+++ resources/views/admin/pages/product/tress/list-tress.blade.php
@@ -193,8 +193,8 @@
 <!-- Include jQuery before your custom script -->
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <!-- Latest compiled and minified JavaScript -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+{{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script> --}}
     <script>
         $('.delete-btn').click(function(e) {
     
